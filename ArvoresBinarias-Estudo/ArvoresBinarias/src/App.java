3299f2226c1590b89cfd89a2ccd95497b61e369b
José Rafael <81492148+JosRafael@users.noreply.github.com>
2022-08-18 14:27:01 -0300

    Add files via upload

---
+++ ArvoresBinarias-Estudo/ArvoresBinarias/src/App.java
@@ -0,0 +1,27 @@
+
+
+public class App {
+
+	 public static void main(String[] args) {
+	    
+		 ArvoresBinarias tree = new ArvoresBinarias();
+	       tree.insere(10);
+	       tree.insere(2);
+	       tree.insere(80);
+	       tree.insere(3);
+	       tree.insere(11);
+	       
+	
+	      
+	       System.out.println("Elemento encontrado?"+tree.busca( 3).getChave());
+	       
+	    
+	    
+	       
+	    
+	    
+	     
+	        
+	        
+	    }
+	}
---
+++ ArvoresBinarias-Estudo/ArvoresBinarias/src/No.java
@@ -0,0 +1,43 @@
+public class No {
+    private int chave;  //chave
+    private No esq;     // ponteiro para o filho esquerdo
+    private No dir;     // ponteiro para o filho direito
+    
+    /**
+     * Construtor que recebe a nova chave por parametro     
+     */
+    public No(int chave)
+    {
+        this.chave = chave;
+        this.esq = null;
+        this.dir = null;
+    }
+    
+            
+    /**
+     * Setters e Getters
+     */
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+
+    public int getChave() {
+        return chave;
+    }
+
+    public void setDir(No dir) {
+        this.dir = dir;
+    }
+
+    public No getDir() {
+        return dir;
+    }
+
+    public void setEsq(No esq) {
+        this.esq = esq;
+    }
+
+    public No getEsq() {
+        return esq;
+    } 
+}
